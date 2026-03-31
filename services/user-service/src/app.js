c2e0d1fb88b8f90114872bbfcfa789dd475d7b13
Miguel Becerra <contact.lmbecerra@gmail.com>
2026-03-30 19:51:10 -0500

    Creacion microservicios faltantes

---
+++ services/user-service/src/app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+
+// Middlewares
+app.use(cors());
+app.use(express.json());
+
+// Rutas
+const authRoutes = require('./routes/auth.routes');
+app.use('/api/auth', authRoutes);
+
+// Ruta de prueba
+app.get('/', (req, res) => {
+  res.send('Auth Service funcionando 🔐');
+});
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Auth Service corriendo en http://localhost:${PORT}`);
+});
