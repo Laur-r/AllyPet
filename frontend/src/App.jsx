fbef8132d12512a96b7e3d61f6f36d520a8d2eb8
Laura <laura.lopez.rodriguez@correounivalle.edu.co>
2026-04-01 21:41:13 -0500

    feat:add Navbar component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,11 @@
 import { useState } from 'react';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
-
-  const [mensaje, setMensaje] = useState("");
-
-  const crearUsuario = () => {
-    fetch('http://localhost:3000/api/users', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        nombre: "Valentina",
-        email: "vale@test.com",
-        password: "123456"
-      })
-    })
-      .then(res => res.json())
-      .then(data => {
-        setMensaje(data.message);
-      })
-      .catch(error => {
-        console.error(error);
-        setMensaje("Error al crear usuario ❌");
-      });
-  };
-
   return (
-    <div>
-      <h1>AllyPet 🐾</h1>
-
-      <button onClick={crearUsuario}>
-        Crear usuario
-      </button>
-
-      <p>{mensaje}</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
