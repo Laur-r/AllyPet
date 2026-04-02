32a23da1cc67d2b740e89a9f0dcf63e83ab7badc
valen <laura.val.dev21@gmail.com>
2026-04-01 21:03:27 -0500

    Frontend del formulario del registro del veterinario

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MenuRegistro      from './pages/MenuRegistro';
 import RegistroDueno     from './pages/RegistroDueno';
 import RegistroPaseador  from './pages/RegistroPaseador';
+import RegistroVeterinario from './pages/RegistroVeterinario';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/register"           element={<MenuRegistro />} />
         <Route path="/register/dueno"     element={<RegistroDueno />} />
         <Route path="/register/paseador"  element={<RegistroPaseador />} />
+        <Route path="/register/veterinario" element={<RegistroVeterinario />} />
       </Routes>
     </BrowserRouter>
   );
