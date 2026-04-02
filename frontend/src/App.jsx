b87fcf76c8f74efc72267749f65f5c118a293b4b
Jean Pierre <jeanphierrem@gmail.com>
2026-04-01 23:01:21 -0500

    feat(frontend): agregar login y assets de login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,14 @@ import MenuRegistro      from './pages/MenuRegistro';
 import RegistroDueno     from './pages/RegistroDueno';
 import RegistroPaseador  from './pages/RegistroPaseador';
 import RegistroVeterinario from './pages/RegistroVeterinario';
+import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/"                   element={<Login />} />
+        <Route path="/login"              element={<Login />} />
         <Route path="/register"           element={<MenuRegistro />} />
         <Route path="/register/dueno"     element={<RegistroDueno />} />
         <Route path="/register/paseador"  element={<RegistroPaseador />} />
